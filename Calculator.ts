245c4b6a190c9b3a631cc5b556ccfcf500fccdf4
Nishant Arora <nish54@gmail.com>
2019-12-27 00:52:34 -0500

    added driver

---
+++ Calculator.ts
@@ -0,0 +1,25 @@
+// Driver
+import {Expense} from "./Expense";
+import {Category} from "./Category";
+
+let categories: Array<Category> = []
+
+categories.push(
+    new Category('Insurance', new Array<Expense>(
+        new Expense('Healthcare', 100)
+    )),,
+
+    new Category('Subscriptions', new Array<Expense>(
+        new Expense("Spotify", 10),
+        new Expense("Hulu", 12.18)
+    ))
+)
+
+categories.forEach(function (category) {
+    console.log(category.category)
+    category.expenses.forEach(function (expense) {
+        console.log('  ' + expense.name)
+        console.log('    ' + expense.amount)
+    })
+});
+
